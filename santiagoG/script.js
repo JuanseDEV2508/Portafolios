d8e3a4359aa5c9f602fe7cc6e22478c2a5c11ff2
Santiago Gomez <gomezsanti69@gmail.com>
2024-10-19 00:45:19 -0500

    Actualizacion de formulario con js

--- santiagoG/script.js
+++ santiagoG/script.js
@@ -19,53 +19,56 @@ function cambiarColor() {
 
 const mostrarAlerta = () =>{(alert("¿Estas seguro de enviar los datos?"))}
 
+     
+//<------------------------------------------------------------------------------------------->
 //Formulario
 
 document.addEventListener("DOMContentLoaded", () =>{
     const form = document.getElementById("formulario-contacto");
-    const nombre = document.getElementById("nombre-contacto");
-    const email = document.getElementById("email-contacto");
-    const asunto = document.getElementById("asunto-contacto");
-    const mensajeExi = document.getElementById("envio-mensaje");
+    const nombre = document.getElementById("form-nombre");
+    const email = document.getElementById("form-email");
+    const asunto = document.getElementById("form-text");
+    const mensajeExi = document.getElementById("mostrarMensaje");
 
     form.addEventListener("submit", (event)=>{
         event.preventDefault();
 
         //Bandera de comprobaciòn
         let isValido = true;
+        
         if(nombre.checkValidity()){
-            document.getElementById("nombreError").style.display = "block";
-            nombre.setAttribute("aria-invalid, true")
-            isValido = false;
-        }else{
             document.getElementById("nombreError").style.display = "none";
-            nombre.setAttribute("aria-invalid, false")
+            nombre.setAttribute("aria-invalid", "true")
+            isValido = true;
+        }else{
+            document.getElementById("nombreError").style.display = "block";
+            nombre.setAttribute("aria-invalid", "false")
             isValido = false;
         }
 
         if(email.checkValidity()){
-            document.getElementById("emailError").style.display = "block";
-            email.setAttribute("aria-invalid, true")
-            isValido = false;
-        }else{
             document.getElementById("emailError").style.display = "none";
-            email.setAttribute("aria-invalid, false")
+            email.setAttribute("aria-invalid", "true")
+            isValido = true;
+        }else{
+            document.getElementById("emailError").style.display = "block";
+            email.setAttribute("aria-invalid", "false")
             isValido = false;
         }
 
         if(asunto.checkValidity()){
-            document.getElementById("asuntoError").style.display = "block";
-            asunto.setAttribute("aria-invalid, true")
-            isValido = false;
+            document.getElementById("textError").style.display = "none";
+            asunto.setAttribute("aria-invalid", "true")
+            isValido = true;
         }else{
-            document.getElementById("asuntoError").style.display = "none";
-            asunto.setAttribute("aria-invalid, false")
+            document.getElementById("textError").style.display = "block";
+            asunto.setAttribute("aria-invalid", "false")
             isValido = false;
         }
 
         if(isValido){
-            from.reset();
-            mensajeExi.style.display = "block;"; //Muestra el mensaje
+            form.reset();
+            mensajeExi.style.display = 'block'; //Muestra el mensaje
         }
 
     })
